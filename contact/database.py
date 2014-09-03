fe506ad9e3c034504b5b1023269f59b9a878eb89
nidhin Bose <chickkunithinbose@gmail.com>
2014-09-03 22:56:01 +0530

    Update database.py

--- contact/database.py
+++ contact/database.py
@@ -13,7 +13,6 @@ def init_db(app):
     app.config['DATABASE_ENGINE'] = engine
     app.config['DATABASE'] = db_session
     Base.query = db_session.query_property()
-    db_session.commit()
     # import all modules here that might define models so that they will be registered properly on the metadata.
     # Otherwise  you will have to import them first before calling init_db()
     import models
